969dba771b4b3fc5e0121027a08809771eaf3cf8
joeythornberry <lewis.thornberry@christendom.edu>
2023-06-02 22:59:55 -0400

    adds conveniece program to close emulator

---
+++ development-close_emulator.py
@@ -0,0 +1,5 @@
+import emulator_handler
+
+#closes all emulator stuff (just for development, not used by program)
+emulator_handler = emulator_handler.EmulatorHandler()
+emulator_handler.close_emulator()
